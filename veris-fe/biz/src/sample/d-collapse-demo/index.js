c84c4bb6b56cc85bf218dc8f94e0c70226ed60bb
qxm_idance <957696990@qq.com>
2016-09-14 15:26:33 +0800

    9.14

---
+++ veris-fe/biz/src/sample/d-collapse-demo/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Button from 'taurus/components/button/button';
+import Collapse from 'taurus/directives/d-collapse/d-collapse';
+
+// Vue.use(Collapse);
+
+new Vue({
+  el: 'body',
+  directives: {
+    Collapse
+  },
+  components: {
+    't-button': Button
+  },
+  props: {
+    expanded: {
+      type: Boolean,
+      twoway: true,
+      default: true
+    }
+  },
+  data: {
+    btnText: '展开'
+  },
+  methods: {
+    expandedVal: function () {
+      this.expanded = !this.expanded;
+    }
+  },
+  watch: {
+    'expanded': function (val) {
+      this.btnText = val ? '展开' : '收起';
+    }
+  }
+});
---
+++ veris-fe/taurus/src/directives/collapse/collapse.vue
@@ -0,0 +1,26 @@
+<script>
+  import Vue from 'vue';
+
+  export default {
+    twoWay: true,
+    bind () {
+    },
+    update (value) {
+      var el = this.el;
+      this._setAriaExpend(el,value);
+      this._setDisplay(el,value);
+    },
+    _setAriaExpend (el, value) {
+      el.setAttribute('aria-expanded', value);
+    },
+    _setDisplay (el,value) {
+      let style = document.createElement('style');
+      let hei = el.offsetHeight;
+      if(value){
+        el.style.cssText = `display:block;`;
+      }else{
+        el.style.cssText = `display:none;`;
+      }
+    }
+  }
+</script>
